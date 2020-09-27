b0d6a3c611e54a9ebf32ebe1afb52fc37a9dd29e
ijounjalkar <jounjalkarisha@gmail.com>
2020-09-27 23:06:14 +0530

    UC3 commited

--- snlstart.java
+++ snlstart.java
@@ -1,6 +1,19 @@
-public class StartPosition{
-public static void main(String[] args) {
-int position=0;
-		System.out.println("Start Position 0");
-}
+public class optioncheck {
+       
+	public static void main(String[] args) {
+		int start=0;
+		int num = (int)Math.floor(Math.random()*10)%6 +1;
 	}
+       switch (num){
+            case 0:
+                 start+=0;
+                 break;
+            case 1:
+                 start+=num;
+                 break;
+            case 2:
+                 start-=num;
+                 break; 
+                 
+}
+}
